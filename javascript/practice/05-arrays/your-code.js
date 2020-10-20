9cc4b393a1cd32d0d374178832c8c68daec10c3a
William King <williamking9772@gmail.com>
2020-10-20 12:28:01 +0100

    Done CODE PRACTICE: Arrays

--- javascript/practice/05-arrays/your-code.js
+++ javascript/practice/05-arrays/your-code.js
@@ -8,21 +8,32 @@
 // Find the maximum number in the array and return it.
 // Tip: Start with 2 numbers in the array. Once the function works with 2 numbers, add a third, and so on.
 // Write your code here:
+function findMax(numbers) {
+    var maxNum;
 
+    for (var i = 0; i < numbers.length; i++) {
+        if (maxNum === undefined) {
+            maxNum = numbers[i];
+        } else if (numbers[i] > maxNum) {
+            maxNum = numbers[i];
+        }
+    }
+    return maxNum;
+}
 
 // Open index.html in your browser, open the JavaScript console, confirm the result.
 
 // Uncomment the code below to verify your function is working.
-// console.log("findMax =====");
-// var numbers = [46, 3, 40, 74, 63];
-// var actual = findMax(numbers);
-// console.log("[46, 3, 40, 74, 63] -> expected: 74, actual: %s", actual);
-// numbers = [3, 3, 3, 3];
-// actual = findMax(numbers);
-// console.log("[3, 3, 3, 3] -> expected: 3, actual: %s", actual);
-// numbers = [-1];
-// actual = findMax(numbers);
-// console.log("[-1] -> expected: -1, actual: %s", actual);
+console.log("findMax =====");
+var numbers = [46, 3, 40, 74, 63];
+var actual = findMax(numbers);
+console.log("[46, 3, 40, 74, 63] -> expected: 74, actual: %s", actual);
+numbers = [3, 3, 3, 3];
+actual = findMax(numbers);
+console.log("[3, 3, 3, 3] -> expected: 3, actual: %s", actual);
+numbers = [-1];
+actual = findMax(numbers);
+console.log("[-1] -> expected: -1, actual: %s", actual);
 
 
 // 2. COMBINE ARRAYS
@@ -31,17 +42,30 @@
 // The function should create a new, empty array and then add all elements from each parameter array to it.
 // The function should NOT alter either parameter.
 // Write your code here:
+function combine(array1, array2) {
+    var newArray = Array();
 
+    for (var i = 0; i < array1.length; i++) {
+        newArray.push(array1[i]);
+    }
+
+    for (var i = 0; i < array2.length; i++) {
+        newArray.push(array2[i]);
+    }
+    // console.log(newArray);
+    return newArray;
+}
 
 
 // Uncomment the code below to verify your function is working.
-// console.log("combine =====");
-// var merged = combine(["Oak", "Maple", "Birch"], ["Hydrogen", "Helium"]);
-// console.log("expected: Oak,Maple,Birch,Hydrogen,Helium\nactual: %s", merged);
-// merged = combine([12.5, 3, 855], [true, false, true]);
-// console.log("expected: 12.5,3,855,true,false,true\nactual: %s", merged);
-// merged = combine(["Saturn"], ["Black Lab", "Borzoi", "Pug"]);
-// console.log("expected: Saturn,Black Lab,Borzoi,Pug\nactual: %s", merged);
+// NOTE: Changed the console logs to show array contents instead of "Array(ARRAY LENGTH)".
+console.log("combine =====");
+var merged = combine(["Oak", "Maple", "Birch"], ["Hydrogen", "Helium"]);
+console.log("expected: Oak,Maple,Birch,Hydrogen,Helium\nactual: " + merged);
+merged = combine([12.5, 3, 855], [true, false, true]);
+console.log("expected: 12.5,3,855,true,false,true\nactual: " + merged);
+merged = combine(["Saturn"], ["Black Lab", "Borzoi", "Pug"]);
+console.log("expected: Saturn,Black Lab,Borzoi,Pug\nactual: " + merged);
 
 
 // 3. CHALLENGE: SUBTRACT AN ARRAY
@@ -52,17 +76,39 @@
 // from the first and return the result.
 // The function should NOT alter either parameter.
 // Write your code here:
+function subtractArray(array1, array2) {
+    var newArray = Array();
 
+    // Copy contents of the main array to the newArray. 
+    for (var i = 0; i < array1.length; i++) {
+        newArray.push(array1[i]);
+    }
+
+    // Remove any matching elements from the newArray.
+    for (var i = 0; i < array2.length; i++) {
+        for (var j = 0; j < newArray.length; j++) {
+            if (newArray[j] === array2[i]) {
+                // Remove the matching element.
+                newArray.splice(j, 1);
+                // Reset the index to the beginning.
+                // NOTE: It's possible to skip an element without resetting the index.
+                // Set to -1 because it will increment to 0 before the next iteration.
+                j = -1; 
+            }
+        }
+    }
+    return newArray;
+}
 
 
 // Uncomment the code below to verify your function is working.
-// console.log("subtractArray =====");
-// var result = subtractArray([1, 2, 3, 4, 5], [2, 4, 6]);
-// console.log("expected: 1,3,5\nactual: %s", result);
-// result = subtractArray(["Neptune", "Uranus", "Saturn", "Pluto"], ["Pluto"]);
-// console.log("expected: Neptune,Uranus,Saturn\nactual: %s", result);
-// result = subtractArray([1, 2, 3, 1, 2, 3, 1, 2, 3], [1, 3]);
-// console.log("expected: 2,2,2\nactual: %s", result);
+console.log("subtractArray =====");
+var result = subtractArray([1, 2, 3, 4, 5], [2, 4, 6]);
+console.log("expected: 1,3,5\nactual: " + result);
+result = subtractArray(["Neptune", "Uranus", "Saturn", "Pluto"], ["Pluto"]);
+console.log("expected: Neptune,Uranus,Saturn\nactual: " + result);
+result = subtractArray([1, 1, 2, 3, 1, 2, 3, 1, 2, 3], [1, 3]);
+console.log("expected: 2,2,2\nactual: " +  result);
 
 
 
